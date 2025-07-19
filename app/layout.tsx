0521ed19986179be1d75e994c82ba09f8e0592e4
JP Mondal <jpmondal2003@gmail.com>
2025-07-19 12:53:20 +0600

    clerk fully removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
   title: {
@@ -18,14 +17,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className="font-poppins antialiased">
-          <Header />
-          {children}
-          <Footer />
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body className="font-poppins antialiased">
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
   );
 }
